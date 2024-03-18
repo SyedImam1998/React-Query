1e3a53bbd5601f58cc7060bd9465aa3ff2f1588f
syed imam <syedimam1998@gmail.com>
2024-03-18 22:29:54 +0530

    working

--- React-Query/src/Screen/RqSuperHero.jsx
+++ React-Query/src/Screen/RqSuperHero.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
-const RqSuperHero = () => {
-  return (
-    <div>RqSuperHero</div>
-  )
+const fetchData=()=>{
+    return axios.get("http://localhost:4000/superheroes");
 }
+const RqSuperHero = () => {  
+  const { isLoading, data, isError,error } = useQuery("super-heros", fetchData,{staleTime:5000});
 
-export default RqSuperHero
+  if (isLoading) {
+    return "Loading";
+  }
+  if(isError){
+    return error.message;
+  }
+  return (
+    <div>
+      {data?.data.map((item) => {
+        return <li key={item.id}>{item.name}</li>;
+      })}
+    </div>
+  );
+};
+
+export default RqSuperHero;
